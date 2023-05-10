885f41968ff82412c5da18a66703f831042a96bc
seclair <appc0derseclair@gmail.com>
2023-05-10 19:18:25 +0200

    Advance ToDoElement to get ready for use with Rooms

--- app/src/main/java/com/example/todoapp/data/todoelement.kt
+++ app/src/main/java/com/example/todoapp/data/todoelement.kt
@@ -1,5 +1,12 @@
 package com.example.todoapp.data
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
 data class ToDoElement (
-    val string: String
+    @PrimaryKey(autoGenerate = true)
+    val id: Int,
+    val description: String,
+    val status: Int
 )
