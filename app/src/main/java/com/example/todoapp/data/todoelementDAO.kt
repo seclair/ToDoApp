7c565cf7f3ecfed136b27d9ca37b4fd60c7b2bf8
seclair <appc0derseclair@gmail.com>
2023-05-20 18:06:51 +0200

    Added "Update" to todoelementDAO

--- app/src/main/java/com/example/todoapp/data/todoelementDAO.kt
+++ app/src/main/java/com/example/todoapp/data/todoelementDAO.kt
@@ -5,6 +5,7 @@ import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import androidx.room.Update
 import kotlinx.coroutines.flow.Flow
 
 @Dao
@@ -15,6 +16,9 @@ interface todoelementDAO {
 
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     suspend fun insert(todoelement: ToDoElement)
+
+    @Update
+    suspend fun update(toDoElement: ToDoElement)
 
     @Delete
     suspend fun delete(toDoElement: ToDoElement)
