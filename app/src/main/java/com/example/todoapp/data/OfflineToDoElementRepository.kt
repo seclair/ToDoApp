833da1cd05344245e5ed69e5e3c88f9edea9f9aa
seclair <appc0derseclair@gmail.com>
2023-05-20 18:56:25 +0200

    override funktions created

--- app/src/main/java/com/example/todoapp/data/OfflineToDoElementRepository.kt
+++ app/src/main/java/com/example/todoapp/data/OfflineToDoElementRepository.kt
@@ -2,6 +2,9 @@ package com.example.todoapp.data
 
 import kotlinx.coroutines.flow.Flow
 
-class OfflineToDoElementRepository(private val todoelementDAO: todoelementDAO): ToDoElementRepository {
-
+class OfflineToDoElementRepository(private val toDoElementDAO: ToDoElementDAO): ToDoElementRepository {
+    override fun getAllToDoElements(): Flow<List<ToDoElement>> = toDoElementDAO.getAllToDos()
+    override suspend fun insertToDoElement(toDoElement: ToDoElement) = toDoElementDAO.insert(toDoElement)
+    override suspend fun updateToDoElement(toDoElement: ToDoElement) = toDoElementDAO.update(toDoElement)
+    override suspend fun deleteToDoElement(toDoElement: ToDoElement) = toDoElementDAO.delete(toDoElement)
 }
