52dacb4b31e7db46451e1bc67a9f74213c8a58b1
seclair <appc0derseclair@gmail.com>
2023-05-20 19:37:39 +0200

    Fixed Name Error

--- app/src/main/java/com/example/todoapp/data/AppContainer.kt
+++ app/src/main/java/com/example/todoapp/data/AppContainer.kt
@@ -3,7 +3,7 @@ package com.example.todoapp.data
 import android.content.Context
 
 interface AppContainer {
-    val toDoElementRepositoryRepository: ToDoElementRepository
+    val toDoElementRepository: ToDoElementRepository
 }
 
 class AppDataContainer (private val context: Context) : AppContainer {
